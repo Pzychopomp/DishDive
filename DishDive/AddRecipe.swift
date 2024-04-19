9f89829a61cae57324252375445db252c387a873
David Nguyen <dnguyen271@csu.fullerton.edu>
2024-04-18 21:59:17 -0700

    Changed layout of AddRecipe.

--- DishDive/AddRecipe.swift
+++ DishDive/AddRecipe.swift
@@ -10,57 +10,60 @@ import SwiftUI
 struct AddRecipe: View {
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        GeometryReader { _ in
-            VStack{
-                HStack{
-                    Spacer()
-                    Button("Done") {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }
-                    .padding(10) // Padding inside the link
-                    .frame(width: 80, height: 50) // Size of the clickable area
-                    .background(Color.gray) // Background color of the button
-                    .cornerRadius(10) // Rounded corners
-                    Spacer().frame(width: 20)
-                }
-                VStack{
-                    HStack{
+        GeometryReader { geometry in
+            VStack {
+                VStack {
+                    HStack {
                         Text("Recipe Name")
-                            .frame(width:130, height: 40)
+                            .frame(alignment: .leading)
                         Spacer()
-                        Text("Input")
-                            .frame(width:180, height: 40)
-                            .background(Color.indigo)
-                            .cornerRadius(7)
                     }
-                    Spacer()
-                    HStack{
-                        Text("Ingredients")
-                            .frame(width:130, height: 20)
-                        Spacer()
-                        Text("Input")
-                            .frame(width:180, height: 250)
-                            .background(Color.indigo)
-                            .cornerRadius(7)
-                    }
-                    Spacer()
-                    HStack{
-                        Text("Steps")
-                            .frame(width:130, height: 20)
-                        Spacer()
-                        ScrollView{
-                            Text("Step 1")
-                        }
-                        .frame(width:180, height: 300)
+                    Text("Input")
+                        .frame(width: geometry.size.width/1.2, height: geometry.size.height/25)
                         .background(Color.indigo)
                         .cornerRadius(7)
-                    }
                 }
-                .padding()
-                .frame(width: 350, height: 640)
-                .background(Color.gray)
-                .cornerRadius(15)
+                Spacer()
+                VStack {
+                    HStack {
+                        Text("Ingredients")
+                            .frame(alignment: .leading)
+                        Spacer()
+                    }
+                    Text("Input")
+                        .frame(width: geometry.size.width/1.2, height: geometry.size.height/4.5, alignment: .center)
+                        .background(Color.indigo)
+                        .cornerRadius(7)
+                }
+                Spacer()
+                VStack {
+                    HStack {
+                        Text("Steps")
+                            .frame(alignment: .leading)
+                        Spacer()
+                    }
+                    ScrollView {
+                        Text("Step 1")
+                    }
+                    .frame(width: geometry.size.width/1.2, height: geometry.size.height/2.5, alignment: .center)
+                    .background(Color.indigo)
+                    .cornerRadius(7)
+                }
+                Spacer()
+                Button("Done") {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+                .padding(10) // Padding inside the link
+                .frame(width: geometry.size.width/1.5, height: geometry.size.height/10, alignment: .center)
+                .background(Color.black) // Background color of the button
+                .cornerRadius(10) // Rounded corners
+                Spacer()
             }
+            .padding()
+            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+            .background(Color.gray)
+            .cornerRadius(15)
         }
+        .padding()
     }
 }
